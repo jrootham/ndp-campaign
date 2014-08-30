f836108fb89d9d89b96790558abd8f477f298139
jrootham <jrootham@gmail.com>
2014-08-30 01:54:17 -0400

    Start adding Assign

--- adminSrc/assignJSX.js
+++ adminSrc/assignJSX.js
@@ -8,8 +8,46 @@
 (function () {
     "use strict"
 
-    var main = function(depends) {
+    var React = require("react")
+    var common = require("../js/commom")
+    var permissions = require("./PermissionsJSX")
 
+    var localDepends
+    var Permissions = permissions.Permissions
+
+    var Question = React.createClass({
+        render: function() {
+            return(
+                <div>
+                Question
+                </div>
+                )
+        }
+    })
+
+    var Assign = React.createClass({
+        render: function() {
+            return (
+                <div>
+                    <div>
+                        <Permission />
+                    </div>
+                    <div>
+                        <Question />
+                    </div>
+                </div>
+            )
+        }
+    })
+
+    var render = function(value) {
+        React.renderComponent(<Assign />, document.getElementById('body'))
+    }
+
+    var main = function(depends) {
+        localDepends = depends
+        permissions.getRootArray()
+        render()
     }
 
     exports.main = main
